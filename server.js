a6ebb372e2818514f52665cc3ac77f6b1cd2b064
cold <dandeliontin@gmail.com>
2014-04-07 13:43:49 +0800

    mongolab

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var user = require('./app/routes/user');
 
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://heroku_app23799987:rihamjd7tgdbf4q817gdrq2pr0@ds049237.mongolab.com:49237/heroku_app23799987');
+mongoose.connect('mongodb://heroku_app23838631:92u82m6320u932rmbk6ebl404h@ds045897.mongolab.com:45897/heroku_app23838631');
 //mongoose.connect('mongodb://localhost/db');
 
 var http = require('http');
