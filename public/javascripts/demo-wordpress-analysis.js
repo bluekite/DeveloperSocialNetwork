ffe2dfab94d0b485bd2b8ce21a683174a3662754
cold <490989971@qq.com>
2014-04-30 13:43:08 +0800

    single&multiple

--- public/javascripts/demo-wordpress-analysis.js
+++ public/javascripts/demo-wordpress-analysis.js
@@ -1,7 +1,23 @@
 $(document).ready(function(){
+
+	renderCircleGraph("/wordpress/circle_developer_comment_15.json","developer-comment",100);
+	renderCircleGraph("/wordpress/circle_developer_commit_15.json","developer-commit",100);
+	renderCircleGraph("/wordpress/circle_developer_work_15.json","developer-work",100);
+
+	//renderCircleGraph("/wordpress/circle_file_logic_15.json","file-logic",10);
+	//renderCircleGraph("/wordpress/circle_file_syntax_15.json","file-syntax",10);
+	//renderCircleGraph("/wordpress/circle_file_work_15.json","file-work",10);
+
+	renderCircleGraph("/wordpress/circle_TT_logic_15.json","TT-logic",250);
+	renderCircleGraph("/wordpress/circle_TT_syntax_15.json","TT-syntax",150);
+	renderCircleGraph("/wordpress/circle_TT_work_15.json","TT-work",100);
+
+
+	var table_base = '<tr><th>Developer Name</th><th>logic.DegreeCentrality</th><th>syntax.DegreeCentrality</th><th>work.DegreeCentrality</th></tr>';
+
 	$.getJSON('/analysis/wordpress/15', function(ANS) {
 		var table_html = '';
-		table_html += '<tr><th>Developer Name</th><th>logic.DegreeCentrality</th><th>syntax.DegreeCentrality</th><th>work.DegreeCentrality</th></tr>';
+		table_html += table_base;
 		for( var i=0;i < ANS.DegreeCentrality.length; i++){
 			table_html += '<tr><td>'+ ANS.DegreeCentrality[i].name +'</td><td>'+ ANS.DegreeCentrality[i].logic +'</td><td>'+ ANS.DegreeCentrality[i].syntax +'</td><td>'+ ANS.DegreeCentrality[i].work +'</td><td>'+'</td></tr>';
 		}
@@ -18,6 +34,21 @@ $(document).ready(function(){
 
 			$('#TT-analysis').html(table_html);
 		});
-	})
+	});
+
+	$('label.version').on('click',function(event) {
+		renderMainGraph("/wordpress/circle_"+($('label.TT.active').html().split("<")[0])+"_"+$(this).html().split("<")[0]+".json","graph",100);
+		console.log($(this).html().split("<")[0]);
+	});
+
+	$('label.TT').on('click',function(event) {
+		renderMainGraph("/wordpress/circle_"+($(this).html().split("<")[0])+"_"+$('label.version.active').html().split("<")[0]+".json","graph",100);
+		console.log($(this).html().split("<")[0]);
+	});
+	$('label.developer').on('click',function(event) {
+		renderMainGraph("/wordpress/circle_"+($(this).html().split("<")[0])+"_"+$('label.version.active').html().split("<")[0]+".json","graph",100);
+		console.log($(this).html().split("<")[0]);
+	});
+
 
 })
--- public/javascripts/demo-wordpress-graph.js
+++ public/javascripts/demo-wordpress-graph.js
@@ -16,6 +16,87 @@ function selectVersion(){
     renderCircleGraph("/wordpress/circle_TT_work_" + this.options[this.selectedIndex].value + ".json","TT-work",100);
 };
 
+var renderMainGraph = function( jsonFile, divId, distance){
+
+    var width = 1000,
+        height = 600;
+
+    var color = d3.scale.category20();
+
+    var force = d3.layout.force()
+        .charge(-120)
+        .linkDistance(distance)
+        .size([width, height]);
+
+    /*clear the graph out*/
+    d3.select(document.getElementById(divId)).html("<label>"+divId+"-netwrok</label>");
+
+    var svg = d3.select(document.getElementById(divId)).append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+
+    d3.json(jsonFile, function(error, graph) {
+        force
+            .nodes(graph.nodes)
+            .links(graph.links)
+            //.text(graph.nodes)
+            .start();
+
+        var link = svg.selectAll(".link")
+            .data(graph.links)
+            .enter().append("line")
+            .attr("class", "link")
+            .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+        var node = svg.selectAll(".node")
+            .data(graph.nodes)
+            .enter().append("circle")
+            .attr("class", "node")
+            .attr("r", 5)
+            .style("fill", function(d) { return color(d.group); })
+            .call(force.drag);
+
+        // node.append("title")
+        //     .text(function(d) { return d.name; });
+
+        // var g = svg.selectAll("g")
+        //     .data(graph.nodes)
+        //     .enter().append('text')
+        //     .text(function(d) {
+        //         return d.name;
+        //     });
+        var text = svg.selectAll(".text")
+            .data(graph.nodes)
+            .enter().append("text")
+            .style("fill", function(d) { return color(d.group); })
+            .text(function(d) { return d.name ; })
+            .on("click", function(d){ console.log(d.name);})
+            .call(force.drag);
+
+
+        force.on("tick", function() {
+            link.attr("x1", function(d) { return d.source.x; })
+                .attr("y1", function(d) { return d.source.y; })
+                .attr("x2", function(d) { return d.target.x; })
+                .attr("y2", function(d) { return d.target.y; });
+
+            node.attr("cx", function(d) { return d.x; })
+                .attr("cy", function(d) { return d.y; });
+
+            text.attr("x", function(d) { return d.x; })
+                .attr("y", function(d) { return d.y; });
+            
+        });
+    });
+
+    d3.select(".text")
+        .on("click", function() {
+            console.log(this.value);
+        });
+
+}
+
 
 var renderCircleGraph = function( jsonFile, divId, distance){
 
@@ -70,9 +151,9 @@ var renderCircleGraph = function( jsonFile, divId, distance){
         var text = svg.selectAll(".text")
             .data(graph.nodes)
             .enter().append("text")
-            //.attr("class", "node")
-            //.attr("r", 5)
-            .text(function(d) { return d.name})
+            .style("fill", function(d) { return color(d.group); })
+            .text(function(d) { return d.name ; })
+            .on("click", function(d){ console.log(d.name);})
             .call(force.drag);
 
 
@@ -90,6 +171,12 @@ var renderCircleGraph = function( jsonFile, divId, distance){
             
         });
     });
+
+    d3.select(".text")
+        .on("click", function() {
+            console.log(this.value);
+        });
+
 }
 
 var renderLabelGraph = function(jsonFile, divId){
@@ -309,15 +396,9 @@ var renderLabelGraphTest = function(jsonFile, divId){
     });
 }
 
-renderCircleGraph("/wordpress/circle_developer_comment_15.json","developer-comment",100);
-renderCircleGraph("/wordpress/circle_developer_commit_15.json","developer-commit",100);
-renderCircleGraph("/wordpress/circle_developer_work_15.json","developer-work",100);
+var testoutput = function(){
+    console.log("test output");
+}
 
-//renderCircleGraph("/wordpress/circle_file_logic_15.json","file-logic",10);
-//renderCircleGraph("/wordpress/circle_file_syntax_15.json","file-syntax",10);
-//renderCircleGraph("/wordpress/circle_file_work_15.json","file-work",10);
 
-renderCircleGraph("/wordpress/circle_TT_logic_15.json","TT-logic",250);
-renderCircleGraph("/wordpress/circle_TT_syntax_15.json","TT-syntax",150);
-renderCircleGraph("/wordpress/circle_TT_work_15.json","TT-work",100);
 
